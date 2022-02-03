c78400e75b8750f53eeb94b61124c6336dd48c43
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-03 12:25:37 +0100

    Styling main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <main>
+          <h1 className="main--title">Fun facts about React</h1>
+          <ul className="main--facts">
+            <li>Was first realeased in 2013</li>
+            <li>War originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of entreprise apps, including mobile apps</li>
+          </ul>
+        </main>
     )
 }
