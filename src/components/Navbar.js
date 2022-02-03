53832b1cc7ab32933e2ac4c2861a4ec6ff4064eb
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-03 11:27:20 +0100

    Navbar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React logo"></img>
+          <h3 className="nav--logo_text">ReactFacts</h3>
+          <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
